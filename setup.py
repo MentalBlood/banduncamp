4899eea0cf0e2f7f0c2b371d1f717c3ced40b88d
mentalblood <neceporenkostepan@gmail.com>
2022-04-11 19:38:45 +0300

    moved from poetry to setuptools

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+
+	long_description = ''
+	if os.path.exists('README.md'):
+		with open('README.md', encoding='utf-8') as f:
+			long_description = f.read()
+
+	setup(
+		name='banduncamp',
+		version='1.0.0',
+		description='Fast tool for downloading audio from bandcamp',
+		long_description=long_description,
+		long_description_content_type='text/markdown',
+		author='mentalblood',
+		install_requires=[
+			'requests',
+			'tqdm',
+			'mutagen',
+			'python_version >= "3.10"'
+		],
+		packages=find_packages()
+	)
