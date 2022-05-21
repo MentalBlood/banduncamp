0c0554cf7161b006c63c2a306eec4029c0f3e383
mentalblood <neceporenkostepan@gmail.com>
2022-05-21 17:40:53 +0300

    single-relese artists handling

--- banduncamp/URL.py
+++ banduncamp/URL.py
@@ -15,12 +15,31 @@ class URL(str):
 		albums_filter: Callable[[str, str], bool]
 	) -> Album | Artist:
 
-		if 'album' in url.split('/'):
-			composer = Album.fromUrl
-		else:
-			composer = partial(Artist.fromUrl, albums_filter=albums_filter)
+		composers = {
+			'album': Album.fromUrl,
+			'artist': partial(Artist.fromUrl, albums_filter=albums_filter)
+		}
 
-		return composer(
-			url=url,
-			downloader=downloader
-		)
+		if 'album' in url.split('/'):
+			proposed = 'album'
+		else:
+			proposed = 'artist'
+
+		try:
+			result = composers[proposed](
+				url=url,
+				downloader=downloader
+			)
+		except:
+			for another in composers.keys():
+				if another != proposed:
+					try:
+						result = composers[another](
+							url=url,
+							downloader=downloader
+						)
+						return result
+					except:
+						pass
+
+		raise TypeError(f'No composer found among {[*composers.keys()]} for URL: {url}')
